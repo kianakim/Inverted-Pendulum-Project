1ffe8acec8c64365621d3a4419c08ec8b57f5629
Kiana Kim <kianamkim@gmail.com>
2015-06-30 00:25:01 -1000

    updated PID implementation
    following "beginner's PID" guide: added run PID at set intervals, added
    tuning variables, still a little confused how/where to set setpoints.

--- iP_PIDimplement/iP_PIDimplement.ino
+++ iP_PIDimplement/iP_PIDimplement.ino
@@ -1,7 +1,7 @@
 /* Kiana Kim - 6/29/15
  * PID implementation w/ Inverted Pendulum
  * attempt to use PID control to read pendulum position and adjust
- * rate of change with Kp values
+ * change. help from "improving the beginner's pid"
  */
 
 #include <Encoder.h>
@@ -10,9 +10,11 @@ Encoder myEnc (2,3);
 float vout = 190; //about neutral w/ offset
 
 //PID Variables
-unsigned long lastTime
+unsigned long lastTime;
 double input, output, setpoint;
 double kp, ki, kd;
+double errSum, lastError;
+int sampleTime = 1000; //time you want in between each run of PID, 1 sec
 
 void setup() {
   pinMode(6, OUTPUT);
@@ -20,11 +22,30 @@ void setup() {
   Serial.begin(9600);
 }
 
-//Take 
+void Compute() {
+  unsigned long now = millis();
+  
+  int timeChange = (now - lastTime);
+  if(timeChange >= sampleTime) { //sets the PID to run at scheduled interval
+    double error = setpoint - lastError);
+    errSum += error; //errSum = errSum + error
+    double dErr = (error - lastError); 
+    
+    output = kp * error + ki * errSum + kd *dErr; 
+    
+    lastErr = error; //updates error and time
+    lastTime = now;
+    
+void SetTunings(double Kp, double Ki, double Kd) {
+  double SampleTimeInSec = ((double)SampleTime)/1000;
+  kp = Kp;
+  ki = Ki * SampleTimeInSec;
+  kd = Kd / SampleTimeInSec;
+}
+
 void loop() {
   long input = myEnc.read(); //reads encoder value as 'input'
-  setpoint = 
-  
+  //what do I do with the output? where do I put this? how is the setpoint...set?
+  //would it be like:
+  //vout = output; (with limiters 0-255)
 }
-  
-
