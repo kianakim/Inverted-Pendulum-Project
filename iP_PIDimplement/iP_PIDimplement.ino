3c4f383d827e4788f31ede2504943718644db092
Kiana Kim <kianamkim@gmail.com>
2015-07-30 22:50:59 -1000

    7/28/15 - use change setpoint method to center cart
    Learned superimposing PID loops is not the way to go.  new method:
    centerCart() uses 'kc' variable multiplied to cart encoder values to
    keep centered; changed setPower() back to setLimits(); reinstated
    sampleTime to 3 ms

--- iP_PIDimplement/iP_PIDimplement.ino
+++ iP_PIDimplement/iP_PIDimplement.ino
@@ -10,30 +10,21 @@
 Encoder myEnc2 (3,4); //pendulum encoder
 Encoder myEnc (2,5); //cart encoder
 
-float voutP, voutC, motorPower;
+float vout;
 int sampleTime = 3, timeChange; //how often to run Compute(), check encoder
-unsigned long lastTime, lastTimeC;
+unsigned long lastTime;
+double inputC, kc = 0.004; //variable to control setpoint in centerCart() method .004
 
 //pendulum PID variables
 long input, output, error; //output is direct value from PID equation, error is setpoint-input
 int setpoint = 0; //default setpoint is 0, upright - can be changed in changeSetpoint()
-double kp = 200, // 'P' value - 200
-ki = 3, // 'I' value - 3
-kd = 150; // 'D' value - 150
+double kp = 230, // 'P' value - 210
+ki = 6, // 'I' value - 6
+kd = 180; // 'D' value - 150
 long errSum, errArray[100], dErr, dErrArray[5]; //variables used for computing Integral and Derivative parts
 int counter = 0, counter2 = 0; //part of integral calculation
 int controlDirection = 1; //0 = left, 1 = right
 unsigned long now;
-
-//cart PID variables
-long inputC, outputC, errorC;
-int setpointC = 0;
-int counterC = 0, counterC2 = 0;
-double kpC = .4,
-kiC = 0,
-kdC = 0;
-long errSumC, errArrayC[100], dErrC;
-double spk = ; //setpoint variable
 
 void setup() {
   pinMode(6, OUTPUT);
@@ -46,11 +37,11 @@ void loop() {
     counter = 0; 
   determineDirection(); //check direction falling based on encoder value
   resetEncoder(); //reset the encoder to 0 when input >360 degrees
+  centerCart();
   Compute(); //Compute output based on PID algorithm evert 50 ms
-  cartPos(); //Keeps cart in center of gear rack using 2nd encoder
-  setPower(); //add outputs from pendulum and cart PID + limit voltage to 0-255
+  setLimits(); //add outputs from pendulum and cart PID + limit voltage to 0-255
 
-  analogWrite(6,motorPower); //send calculated voltage to power module
+  analogWrite(6,vout); //send calculated voltage to power module
   counter++;
 }
 
@@ -58,7 +49,7 @@ void Compute() {
   now = millis();
   input = myEnc.read(); //read current encoder value
   timeChange = (now - lastTime);
-  //if(timeChange >= sampleTime) { //controls when to run Compute()
+  if(timeChange >= sampleTime) { //controls when to run Compute()
       // 'P' calculation
     error = (setpoint - input);
       
@@ -79,17 +70,22 @@ void Compute() {
 
     output = kp*error + ki*errSum + kd*dErr;
     if(controlDirection == 0)
-      voutP = 210 + output; //changed from 128 because of offset 240
+      vout = 210 + output; //changed from 128 because of offset 240
     if(controlDirection == 1)
-      voutP = -10 + output;
+      vout = -10 + output;
     
     lastTime = now;
-  //}
+  }
 }
 
 void resetEncoder() { //setpoint will change back once it's not 360
   if(abs(input) >= 4095) //3000 encoder counts = about 270 degrees
     myEnc.write(0);
+}
+
+void centerCart() {
+    inputC = myEnc2.read(); //reads cart encoder values
+    setpoint = inputC*kc;
 }
 
 void determineDirection() {
@@ -99,54 +95,21 @@ void determineDirection() {
     controlDirection = 0;
 }
 
-void cartPos() {
-  inputC = myEnc2.read(); //reads cart encoder values
-
-  }
-  /*now = millis();
-  inputC = myEnc2.read(); //reads cart encoder values
-  timeChange = (now - lastTimeC); //timeChange........
-  if(timeChange >= sampleTime) {
-      // 'P' calculation
-    errorC = (setpointC - inputC);
-      
-      // 'I' calculation
-    errSumC = 0; //reset errSum, so it only takes the current 100 values
-    errArrayC[counter] = errorC; //set current error to the n'th element in array
-    int i;
-    for(i=0; i<100; i++) {
-      errSumC += errArrayC[i]; //add past 100 error values
-    }
-    
-      // 'D' calculation
-    dErrC = errorC - errArrayC[abs(counter - 5)];
-
-    outputC = kpC*errorC + kiC*errSumC + kdC*dErrC;
-    voutC = 128 + outputC;
-    /*
-      voutC = 210 + outputC; //changed from 128 because of offset
-    if(inputC > 0)
-      voutC = -10 + outputC;
-    
-    now = lastTimeC;
-  }
-}
-void setPower() {
-  motorPower = voutP + voutC;
-  if(motorPower > 255)
-    motorPower = 255;
-  if(motorPower < 1)
-    motorPower = 1; 
+void setLimits() {
+  if(vout > 255)
+    vout = 255;
+  if(vout < 1)
+    vout = 1; 
   //if(abs(input) > 1000 && abs(input) < 3000)
     //vout = 128;    
-/*
+ /*   
   //print status:
   Serial.print("Encoder Value: ");
   Serial.print(inputC);
-  Serial.print("   Output from PDI: : ");
-  Serial.print(outputC);
-  Serial.print("   Motor Voltage:");
-  Serial.print(motorPower);
+  Serial.print("   Setpoint   ");
+  Serial.print(setpoint);
+  //Serial.print("   kc   ");
+  //Serial.print(kc);
   Serial.print("\n");
 */
 }
